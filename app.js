ae05503d99d7b56fb24bb7fadfade04917feb8ce
katouri-waduda20 <110169939+katouri-waduda20@users.noreply.github.com>
2022-08-12 15:29:05 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const form=document.getElementById("form-container");
+const items=document.getElementById("items"); 
+
+// function that will submit our data
+form.addEventListener("submit",(e)=>{e.preventDefault()
+    let description = form.description.value
+    let amount = form.amount.value
+    generateHTML(description, amount)
+
+} )
+//function that generate HTML structure
+const generateHTML = (description,amount) =>{
+    
+    if(amount < 0 ){
+       let HTML =`
+       <li class="expense">
+       <span>${description}</span>
+       <span>${amount}</span>
+        </li>`
+        items.innerHTML+=HTML
+    }else {
+        let HTML =`
+    <li class="income">
+    <span>${description}</span>
+    <span>${amount}</span>
+     </li>`
+     items.innerHTML+=HTML
+}
+}
